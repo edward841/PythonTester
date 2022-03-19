36782527b70f66ac3a65feafbf0d8085654ac3ab
edward841 <edward.niemann841@gmail.com>
2022-03-19 11:37:29 -0700

    Added cases for some larger fibbonacci numbers

--- testingCode.py
+++ testingCode.py
@@ -12,5 +12,13 @@ class TestFibbonacci(unittest.TestCase):
             with self.subTest(i=i):
                 self.assertEqual(fibbonacci1(i), expected[i-1])
 
+    def test_tens(self):
+        expected = [6765,832040,102334155,12586269025, 1548008755920]
+        for i in range(len(expected)):
+            with self.subTest(i=i):
+                self.assertEqual(fibbonacci1(i*10+20), expected[i])
+
+    def test_huge(self):
+        self.assertEqual(fibbonacci1(100), 354224848179261915075)
 if __name__ == "__main__":
     unittest.main()
